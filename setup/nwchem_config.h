a3e4d036871748a9303c31bbe6a40e2d04017b54
Soumen Ghosh <soumenrkmrc08@gmail.com>
2019-12-12 07:50:34 +0100

    setup

---
+++ setup/nwchem_config.h
@@ -0,0 +1,6 @@
+# This configuration generated automatically on soumen-linux at Sa 26. Okt 09:29:44 CEST 2019
+# Request modules from user: all
+NW_MODULE_SUBDIRS = NWints atomscf ddscf gradients moints nwdft rimp2 stepper driver optim cphf ccsd vib mcscf prepar esp hessian selci dplot mp2_grad qhop property solvation nwpw fft analyz nwmd cafe space drdy vscf qmmm qmd etrans tce bq mm cons perfm dntmc dangchang ccca dimqm semiemp
+NW_MODULE_LIBS = -lccsd -lmcscf -lselci -lmp2 -lmoints -lstepper -ldriver -loptim -lnwdft -lgradients -lcphf -lesp -lddscf -ldangchang -lguess -lhessian -lvib -lnwcutil -lrimp2 -lproperty -lsolvation -lnwints -lprepar -lnwmd -lnwpw -lofpw -lpaw -lpspw -lband -lnwpwlib -lcafe -lspace -lanalyze -lqhop -lpfft -ldplot -ldrdy -lvscf -lqmmm -lqmd -letrans -lpspw -ltce -lbq -lmm -lcons -lperfm -ldntmc -lccca -ldimqm -lsemiemp
+EXCLUDED_SUBDIRS = develop nwxc scfaux rimp2_grad python argos diana uccsdt rism geninterface smd nbo leps lucia rdmft
+CONFIG_LIBS = 
